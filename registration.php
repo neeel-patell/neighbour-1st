aeb43cc7f0d40550d31fb7ade91e90eef32e3790
Neel Patel <neelpatel1905.np@gmail.com>
2020-05-25 16:18:18 +0530

    fix: Registration with image upload

--- registration.php
+++ registration.php
@@ -20,6 +20,13 @@
     $query = "insert into login(first_name,last_name,mobile,email,password,gender,date_of_birth,address,area_id,user_type) values
     ('$first_name','$last_name',$mobile,'$email','$password',$gender,'$date_of_birth','$address',$area,$user_type)";
     if($conn->query($query) == true){
+        $image = $_POST['image'];
+        $query = "select id from login where mobile = $mobile";
+        $result = $conn->query($query);
+        $row = $result->fetch_array();
+        $id = $row['id'];
+        $path = "images/profile/$id.jpg";
+        file_put_contents($path,base64_decode($image));
         echo json_encode(array("message"=>array("success"=>"Successfully Registered")));
     }
     else{
