2d7604a802499e62d95f27d1578209e763e85a27
Neel Patel <neelpatel1905.np@gmail.com>
2020-05-29 15:31:50 +0530

    fix: add store

--- vendor/add_store.php
+++ vendor/add_store.php
@@ -3,13 +3,15 @@
     require '../connection.php';
     require '../mail/mail_sender.php';
     $conn = getConn();
+    $res = array();
     
+    $f = 1;
     $name = $_POST['name'];
     $address = $_POST['address'];
     $category = $_POST['category'];
+    $subcategory = $_POST['subcategory'];
     $area = $_POST['area'];
     $user = $_POST['user'];
-    $subcategory = $_POST['subcategory'];
     $query = "insert into store(name,address,category_id,area_id,user_id) values ('$name','$address',$category,$area,$user)";
     
     if($conn->query($query) == true){
@@ -28,12 +30,14 @@
         $f = 0;
     }
     
-    if($f = 0){
-        echo json_encode(array("message"=>array("error"=>"Store has not registered")));
+    if($f == 0){
+        array_push($res,array("error"=>"Store has not registered"));
+
     }
     else{
         $image = $_POST['image'];
         file_put_contents("../images/vendor/store/$store.jpg",base64_decode($image));
-        echo json_encode(array("message"=>array("success"=>"Store has been registered")));
+        array_push($res,array("success"=>"Store has been registered"));
     }
+    echo json_encode(array("message"=>$res));
 ?>
