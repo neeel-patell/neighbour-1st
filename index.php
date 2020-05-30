50e0c15b1ddb26df97d61601f6bf989143ef84b8
Neel Patel <neelpatel1905.np@gmail.com>
2020-05-30 11:48:11 +0530

    fix: add  store
    - added three more fields
    - subcategory string to array conversion

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		<form method="post" action="varify_email.php">
-			<input type="email" name="email">
-			<input type="submit">
-		</form>
-	</body>
-</html>
--- vendor/add_store.php
+++ vendor/add_store.php
@@ -8,11 +8,21 @@
     $f = 1;
     $name = $_POST['name'];
     $address = $_POST['address'];
+    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
+    $website = $_POST['website'];
     $category = $_POST['category'];
     $subcategory = $_POST['subcategory'];
+    
+    if($subcategory[strlen($subcategory)-1] === ","){
+		$subcategory[strlen($subcategory)-1] = " ";
+		$subcategory = rtrim($subcategory);
+	}
+	$subcategory = explode(",",$subcategory);
+    
     $area = $_POST['area'];
     $user = $_POST['user'];
-    $query = "insert into store(name,address,category_id,area_id,user_id) values ('$name','$address',$category,$area,$user)";
+    $query = "insert into store(name,address,mobile,email,website,category_id,area_id,user_id) values ('$name','$address',$mobile,'$email','$website',$category,$area,$user)";
     
     if($conn->query($query) == true){
         $query = "select id from store where name='$name' and address='$address' and user_id=$user";
@@ -31,7 +41,7 @@
     }
     
     if($f == 0){
-        array_push($res,array("error"=>"Store has not registered"));
+        array_push($res,array("error"=>"Phone number or Email already used for another store"));
 
     }
     else{
