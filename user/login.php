09712f71fb55b654906fe53b19e2c0625460329b
Neel Patel <neelpatel1905.np@gmail.com>
2020-09-07 14:59:19 +0530

    fix: login, registration
    - mail on each login
    - reduced parameter in registration

--- user/login.php
+++ user/login.php
@@ -1,27 +1,34 @@
 <?php
     header('content-type: application/json');
-    require '../connection.php';
+    require_once '../connection.php';
+    require_once '../mail/mail_sender.php';
     $conn = getConn();
 
     $email = $_POST['email'];
     $password = hash("sha256",$_POST['password']);
     
-    $query = "select active,id,first_name,last_name from consumer where email = '$email' and password = '$password'";
+    if(is_numeric($email))
+        $query = "select active,id,first_name,last_name,email,mobile from consumer where mobile = '$email' and password = '$password'";
+    else
+        $query = "select active,id,first_name,last_name,email,mobile from consumer where email = '$email' and password = '$password'";
     $result = $conn->query($query);
     
     if(mysqli_num_rows($result) != 0){
         $row = $result->fetch_array();
         if($row['active'] == 0){
+            sendMail($row['email'],"Login Activity","Your login is just blocked due to unfair or suspecious mean If you want to solve this query then contact support center");
             echo json_encode(array("data"=>array("message"=>"New account will be activated in 24hrs or contact admin")));
         }
         else{
+            $model = $_POST['model'];
+            sendMail($row['email'],"New Login Found","You have just logged in to neighbour-1st app through $model, kindly change the password if that's not you");
             echo json_encode(array("data"=>array("id"=>$row['id'],"first_name"=>$row['first_name'],"last_name"=>$row['last_name'],"message"=>0)));
         }
     }
     else{
         $result = $conn->query("select id from consumer where email = '$email'");
         if(mysqli_num_rows($result) == 0){
-            echo json_encode(array("data"=>array("message"=>"Email Not registered")));
+            echo json_encode(array("data"=>array("message"=>"Email / Mobile Not registered")));
         }
         else{
             echo json_encode(array("data"=>array("message"=>"Wrong Password")));
--- user/registration.php
+++ user/registration.php
@@ -1,6 +1,7 @@
 <?php 
     header('content-type: application/json');
-    include '../connection.php';
+    include_once '../mail/mail_sender.php';
+    include_once '../connection.php';
     $conn = getConn();
     
     $first_name = $_POST['first_name'];
@@ -8,28 +9,15 @@
     $mobile = $_POST['mobile'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $gender = $_POST['gender'];
-    $date_of_birth = $_POST['date_of_birth'];
-    $date_of_birth = str_replace("/","-",$date_of_birth);
-    $address = str_replace("'","\'",$_POST['address']);
-    $area = $_POST['area'];
     
-    $date_of_birth = date("Y-m-d",strtotime($date_of_birth));
     $password = hash("sha256",$password);
     
-    $query = "insert into consumer(first_name,last_name,mobile,email,password,gender,dob,address,area_id) values
-    ('$first_name','$last_name',$mobile,'$email','$password',$gender,'$date_of_birth','$address',$area)";
+    $query = "insert into consumer(first_name,last_name,mobile,email,password) values('$first_name','$last_name',$mobile,'$email','$password')";
     if($conn->query($query) == true){
-        $image = $_POST['image'];
-        $query = "select id from consumer where mobile = $mobile";
-        $result = $conn->query($query);
-        $row = $result->fetch_array();
-        $id = $row['id'];
-        $path = "../images/profile/consumer/$id.jpg";
-        file_put_contents($path,base64_decode($image));
         echo json_encode(array("message"=>array("success"=>"Successfully Registered")));
+        sendMail($email,"New Registration","Thank you for registering on Neighbour-1st app, Hope you will be beneficial from us with our service, Do rating our app if you like our services");
     }
     else{
         echo json_encode(array("message"=>array("error"=>"Email or mobile already registered")));
-    }    
+    }
 ?>
